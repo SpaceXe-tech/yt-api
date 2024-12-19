25051a2305d66ea14c85e085ef95fe0cf1b05375
Simatwa <smartwacaleb@gmail.com>
2024-12-19 03:48:54 +0300

    feat: Update .gitignore to include run.sh and static/media directories fix: Modify app/__init__.py to support downloading YouTube videos in mp4, webm, and mp3 formats

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ from app.v1 import v1_router
 app = FastAPI(
     title="Youtube-Downloader",
     version="0.0.1",
-    summary="Download Youtube videos in mp4 and m4a formats.",
+    summary="Download Youtube videos in mp4, webm and mp3 formats.",
     description="_Under development_",
     terms_of_service="",
     contact={
