6ec6d44c5f04b70da9148a55d24019d4364956bb
Simatwa <smartwacaleb@gmail.com>
2024-12-22 03:30:54 +0300

    feat: Add yt_dlp_bonus functionality for system command execution fix: Improve temp file cleanup process patch: Update event registration logic for startup and shutdown events

--- app/events.py
+++ app/events.py
@@ -4,6 +4,7 @@ from app.utils import create_temp_dirs, download_dir
 from fastapi import FastAPI
 from shutil import rmtree
 from app.db import create_tables
+from yt_dlp_bonus.utils import run_system_command
 
 
 def event_startup_create_tempdirs():
@@ -18,6 +19,11 @@ def event_shutdown_clear_previous_downloads():
     rmtree(download_dir)
 
 
+def event_all_clear_temp_files():
+    run_system_command("rm *.part *.ytdl")
+    run_system_command(f"rm '{download_dir}/*.part' '{download_dir}/*.ytdl'")
+
+
 def register_events(app: FastAPI) -> FastAPI:
     """Sets up event handlers"""
     for var in globals().values():
@@ -25,5 +31,8 @@ def register_events(app: FastAPI) -> FastAPI:
             if var.__name__.startswith("event_startup_"):
                 app.add_event_handler("startup", var)
             elif var.__name__.startswith("event_shutdown_"):
+                app.add_event_handler("shutdown", var)
+            elif var.__name__.startswith("event_all_"):
+                app.add_event_handler("startup", var)
                 app.add_event_handler("shutdown", var)
     return app
