95af82f067577b6068d34913dc2209c64ce043e0
smartwa <smartwacaleb@gmail.com>
2025-01-25 16:49:01 +0300

    feat: Update outtmpl option in ytdl_opts dictionary to include video ID

--- app/v1/routes.py
+++ app/v1/routes.py
@@ -202,7 +202,7 @@ def real_download_process(
     target_format = video_formats.get(payload.quality)
 
     ytdl_opts = {
-        "outtmpl": f"{loaded_config.filename_prefix or ''}{sanitize_filename(extracted_info.title)} (%(format_note)s).%(ext)s"
+        "outtmpl": f"{loaded_config.filename_prefix or ''}{sanitize_filename(extracted_info.title)} (%(id)s, %(format_note)s).%(ext)s"
     }
 
     if loaded_config.embed_subtitles and payload.x_lang is not None:
